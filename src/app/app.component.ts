d0e56170376c8f029ff5d8b75c0da4c020250db1
Vineet Yadav <yadavvineet74@gmail.com>
2019-11-17 22:10:30 +0530

    Committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-﻿import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+﻿import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
 
 import { AuthenticationService } from './_services';
 import { User } from './_models';
@@ -9,7 +10,7 @@ export class AppComponent {
     currentUser: User;
 
     constructor(
-        private router: Router,
+        private router : Router,
         private authenticationService: AuthenticationService
     ) {
         this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
